276f97311222c4ca5831a0b677d6288283d12dee
mostghost <116381662+mostghost@users.noreply.github.com>
2026-02-04 01:24:52 -0500

    Another placeholder update, possibly the last before final asset replacement

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,8 +6,8 @@ export const LIGHT_MODE = "light",
 export const DEFAULT_THEME = DARK_MODE;
 
 // Banner height unit: vh
-export const BANNER_HEIGHT = 70;
-export const BANNER_HEIGHT_EXTEND = 30;
+export const BANNER_HEIGHT = 10;
+export const BANNER_HEIGHT_EXTEND = 85;
 export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
 
 // The height the main panel overlaps the banner, unit: rem
