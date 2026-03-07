30b24e81c8fc030643efc47d672f00092aabedf7
mostghost <116381662+mostghost@users.noreply.github.com>
2026-03-07 10:15:29 -0500

    Little bit of clean-up, moving all images to "public" rather than splitting them between public and src

--- src/config.ts
+++ src/config.ts
@@ -17,8 +17,8 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/banner.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-		bottom: "assets/images/banner-bottom.png",
+		src: "/banner/banner.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		bottom: "/banner/banner-bottom.png",
 		position: "top", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -50,7 +50,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "/avatar/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "M.D.",
 	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	links: [
